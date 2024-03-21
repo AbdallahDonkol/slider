550bbd58d035669c7d1d5919a62f40fc2b725cd5
AbdallahDonkol <abdullahramadan123@gmail.com>
2024-03-21 02:06:22 +0200

    second commit

--- index.js
+++ index.js
@@ -1,55 +1,62 @@
-var allImgs = Array.from(document.querySelectorAll(".item img"));
-var boxContainer = document.querySelector(".lightBoxContainer");
-var lightBox = document.querySelector(".lightBox");
-var rightBtn = document.getElementById("rightBtn");
+var imgs = Array.from(document.querySelectorAll(".item img"));
+var mainImgContainer = document.querySelector(".lightBoxContainer");
+var mainImg = document.querySelector(".lightBox");
+var closeBtn = document.getElementById("closeBtn");
 var leftBtn = document.getElementById("leftBtn");
+var rightBtn = document.getElementById("rightBtn");
 var index;
-for (var i = 0; i < allImgs.length; i++) {
-  allImgs[i].addEventListener("click", function (e) {
-    boxContainer.classList.replace("d-none", "d-flex");
+
+for(var i=0; i<imgs.length;i++ ){
+  imgs[i].addEventListener("click",function(e){
     var currentSrc = e.target.getAttribute("src");
-    lightBox.style.backgroundImage = `url(${currentSrc})`;
-    index = allImgs.indexOf(e.target);
-    if (index == allImgs.length - 1) {
-      rightBtn.classList.add("visually-hidden");
-    }
-    if (index == 0) {
-      leftBtn.classList.add("visually-hidden");
-    }
-  });
+    mainImg.style.backgroundImage = `url(${currentSrc})`;
+    mainImgContainer.classList.replace("d-none","d-flex");
+    index = imgs.indexOf(e.target);
+  })
 }
 
-document.getElementById("closeBtn").addEventListener("click", function () {
-  boxContainer.classList.replace("d-flex", "d-none");
-  if (index < allImgs.length) {
-    rightBtn.classList.replace("visually-hidden", "visible");
-  }
-  if (index >= 0) {
-    leftBtn.classList.replace("visually-hidden", "visible");
-  }
+closeBtn.addEventListener("click",function(){
+  closeSlider();
+})
+
+leftBtn.addEventListener("click",function(){
+  prevSlider()
 });
 
-rightBtn.addEventListener("click", function () {
-  index++;
-  if (index < allImgs.length) {
-    lightBox.style.backgroundImage = `url(${allImgs[index].getAttribute(
-      "src"
-    )})`;
-    leftBtn.classList.replace("visually-hidden", "visible");
-  }
-  if (index == allImgs.length - 1) {
-    rightBtn.classList.add("visually-hidden");
-  }
-});
-leftBtn.addEventListener("click", function () {
+rightBtn.addEventListener("click",function(){
+  nextSlider()
+})
+
+function closeSlider(){
+  mainImgContainer.classList.replace("d-flex","d-none");
+}
+
+function prevSlider(){
   index--;
-  if (index >= 0) {
-    lightBox.style.backgroundImage = `url(${allImgs[index].getAttribute(
-      "src"
-    )})`;
-    rightBtn.classList.replace("visually-hidden", "visible");
+  if(index == -1){
+    index = imgs.length -1;
   }
-  if (index == 0) {
-    leftBtn.classList.add("visually-hidden");
+  mainImg.style.backgroundImage = `url(${imgs[index].getAttribute("src")})`;
+}
+
+function nextSlider(){
+  index++;
+  if(index == imgs.length ){
+    index = 0;
   }
+  mainImg.style.backgroundImage = `url(${imgs[index].getAttribute("src")})`;
+}
+
+document.addEventListener("keyup",function(e){
+  switch(e.code){
+    case "Escape": closeSlider(); break;
+    case "ArrowLeft": prevSlider(); break;
+    case "ArrowRight": nextSlider(); break;
+  } 
 });
+
+mainImgContainer.addEventListener("click", function(e){
+  if(e.target != mainImg && e.target !== leftBtn && e.target != rightBtn){
+    closeSlider();
+  }
+})
